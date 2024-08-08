a21a9f8a2c0b73d7357d5b201f8926933ad10e6c
gangfunction <gangfunction@gmail.com>
2024-08-08 14:53:07 +0900

    mysql 설치

---
+++ src/test/java/com/ecommerce/IndexTest.java
@@ -0,0 +1,23 @@
+package com.ecommerce;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+public class IndexTest {
+    private JdbcTemplate jdbcTemplate;
+    private EmbeddedDatabase db;
+
+    @BeforeEach
+    void setup(){
+        db = new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:data.sql")
+                .build();
+
+    }
+}
