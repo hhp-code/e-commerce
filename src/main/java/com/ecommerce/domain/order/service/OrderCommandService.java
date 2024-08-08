f8446086436b09591bdc62e12d2eeefbae88b8b4
gangfunction <gangfunction@gmail.com>
2024-08-08 17:50:12 +0900

    멘토님 피드백 반영해서 캐싱 어노테이션에서 풋하고 에빅을 조합해줬습니다.

--- src/main/java/com/ecommerce/domain/order/service/OrderCommandService.java
+++ src/main/java/com/ecommerce/domain/order/service/OrderCommandService.java
@@ -5,6 +5,7 @@ import com.ecommerce.interfaces.exception.domain.OrderException;
 import com.ecommerce.domain.order.Order;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,8 +18,10 @@ public class OrderCommandService {
     }
 
     @Transactional
-    @CachePut(value = "orders", key = "#result.id")
-    @CacheEvict(value = "finishedOrders", allEntries = true)
+    @Caching(
+            put = {@CachePut(value = "orders", key = "#result.id")},
+            evict = {@CacheEvict(value = "finishedOrders", allEntries = true)}
+    )
     public Order saveOrder(Order order) {
         return orderCommandRepository.saveAndGet(order)
                 .orElseThrow(() -> new OrderException.ServiceException("주문 저장에 실패하였습니다."));
